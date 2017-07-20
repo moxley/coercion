25cf9d9fc799f68dd0594ec26bbb39b3378094c8
Alexander Merkulov <sasha@merqlove.ru>
2017-07-20 22:03:29 +0300

    use elixir 1.3

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Coercion.Mixfile do
     [
       app: :coercion,
       version: "1.0.0",
-      elixir: "~> 1.4",
+      elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps(),
